2b0ba54a0292448ca0fdd08030af98538a46fd30
mxtmishal <muhammedmishalc.kmechil@gmail.com>
2026-03-05 09:18:22 +0530

    operations

---
+++ hello.py
@@ -0,0 +1,18 @@
+print('hello')
+# name='mishal'
+# age=23
+# is_adult=True
+# print(name)
+# print(age)
+# print(is_adult)
+
+# multiple variables assignments
+name, age, is_adult = 'mishal', 23, True
+print(is_adult)
+print('Age:',age)
+
+# assigning same value to different variables
+x=y=z= 10
+print('x:',x)
+print('y:',x)
+print('z:',x)
---
+++ operations.py
@@ -0,0 +1,40 @@
+# Arithsmatic operators
+
+a=10
+b=5
+p=20
+q=4
+pattern='mishal'
+
+print(a+b) # addition
+print(a-b) # subtraction
+print(a*b) # multiplication
+print(a/b) # division
+print(p//q) # floor division
+print(p%q) # modulus
+print(p**q) # exponential 10*10*10
+print(pattern*3)
+
+# Assignment operators
+
+x=10
+x+=5 # x=x+5
+print('x:',x)
+
+x-=5 # x=x-5
+print('x:',x)
+
+x*=5 # x=x*5
+print('x:',x)
+
+x/=5 # x=x/5
+print('x:',x)
+
+x//=5 # x=x//5
+print('x:',x)
+
+x%=5 # x=x%5
+print('x:',x)
+
+x**=5 # x=x**5
+print('x:',x)
