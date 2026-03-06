55c269970f2319395ef0f7d256e82da020c7e62c
mxtmishal <muhammedmishalc.kmechil@gmail.com>
2026-03-06 09:06:05 +0530

    operations added

--- operations.py
+++ operations.py
@@ -1,40 +1,106 @@
-# Arithsmatic operators
+# Arithsmatic operators//////////////////////////////////////
 
-a=10
-b=5
-p=20
-q=4
-pattern='mishal'
+# a=10
+# b=5
+# p=20
+# q=4
+# pattern='mishal'
 
-print(a+b) # addition
-print(a-b) # subtraction
-print(a*b) # multiplication
-print(a/b) # division
-print(p//q) # floor division
-print(p%q) # modulus
-print(p**q) # exponential 10*10*10
-print(pattern*3)
+# print(a+b) # addition
+# print(a-b) # subtraction
+# print(a*b) # multiplication
+# print(a/b) # division
+# print(p//q) # floor division
+# print(p%q) # modulus
+# print(p**q) # exponential 10*10*10
+# print(pattern*3)
 
-# Assignment operators
+# Assignment operators//////////////////////////////////////
 
-x=10
-x+=5 # x=x+5
-print('x:',x)
+# x=10
+# x+=5 # x=x+5
+# print('x:',x)
 
-x-=5 # x=x-5
-print('x:',x)
+# x-=5 # x=x-5
+# print('x:',x)
 
-x*=5 # x=x*5
-print('x:',x)
+# x*=5 # x=x*5
+# print('x:',x)
 
-x/=5 # x=x/5
-print('x:',x)
+# x/=5 # x=x/5
+# print('x:',x)
 
-x//=5 # x=x//5
-print('x:',x)
+# x//=5 # x=x//5
+# print('x:',x)
 
-x%=5 # x=x%5
-print('x:',x)
+# x%=5 # x=x%5
+# print('x:',x)
 
-x**=5 # x=x**5
-print('x:',x)
+# x**=5 # x=x**5
+# print('x:',x)
+
+
+# Comparison operators/////////////////////////////////////
+
+# a=10
+# b=5
+
+# print(a==b) # equal to
+# print(a!=b) # not equal to
+# print(a> b) # greater than
+# print(a< b) # less than
+# print(a>=b) # greater than or equal to
+# print(a<=b) # less than or equal to
+
+
+# Logical operators///////////////////////////////
+
+x=25
+
+#And
+# print(x<10 and x>5) # and operator
+# print(x<10 and x>5) # and operator
+# print(x>30 and x<20) # and operator
+# print(x>20 and x<50) # and operator
+
+
+#Or
+# print(x>30 or x<20) # or operator
+# print(x<10 or x>5) # or operator
+# print(x>5 or x<10) # or operator
+# print(x>20 or x<50) # or operator
+
+
+
+#Not
+# is_Adult = True
+# print(not(x>30 and x<20)) # not operator
+# print(not(x>20 or x<50)) # not operator
+# print(not is_Adult)
+
+# identity operators //////////////////////////////////
+
+# a=[10,11]
+# b=a
+# c=[10,11]
+
+# print(a is b) 
+# print(a is c)
+# print(a is not c)
+# print(a == c)
+
+
+# membership operators //////////////////////////////////
+
+# print(20 in [12,10,20])
+# print(200 in [12,10,20])
+# print('python'in 'i love python')
+
+# Bitwise operators //////////////////////////////////
+
+print(10 & 5) # bitwise and
+print(10 | 5) # bitwise or
+print(10 ^ 5) # bitwise xor
+print(~10) # bitwise not
+print(10 << 2) # bitwise left shift
+print(10 >> 2) # bitwise right shift
