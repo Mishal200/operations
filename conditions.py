d46cee20125fa40ef625edf7ac2ca139b9da23fd
mxtmishal <muhammedmishalc.kmechil@gmail.com>
2026-03-09 08:56:11 +0530

    conditions added

---
+++ conditions.py
@@ -0,0 +1,59 @@
+# age=7
+
+# if age>=18:
+  #   print("you are eligible for voting")
+# else:
+ #   print("you are not eligible for voting")
+ 
+ # ////////////////////////////////
+ 
+# mark = 95
+
+# if mark>=90:
+  #   print("Grade A")
+# elif mark>=80:
+ #   print("Grade B")
+# elif mark>=70:
+ #   print("Grade C")
+# elif mark>=60:
+  #   print("Grade D")
+# else:
+ #   print("fail")                
+ 
+ 
+ # ///////////////////////////////////////
+ 
+# age = 23
+
+# has_id = True
+
+# if age>=20:
+   #  if has_id:
+      #   print("you are eligible to enter")
+   #  else:
+     #    print("you need to have an id to enter")
+# else:
+  #   print("you are under age to enter")        
+  
+  # //////////////////////////////////////
+  
+  
+age=30
+
+has_id =True
+
+gender ="male"
+
+# if age >18 and has_id:
+  #   print("you are eligible")
+# else:
+  #   print("both conditions are not valid")
+    
+# if age>18 or gender=="male":
+   #  print("you are eligible")
+    
+    
+print(has_id)
+
+if not has_id:
+    print("your id is not valid")
---
+++ task.py
@@ -0,0 +1,19 @@
+number=0
+if number > 0:
+    print("the number is positive")
+elif number < 0:
+    print("the number is negative")
+else :
+    print("the number is zero")
+    
+
+        
+if number % 2 == 0:
+    print("the number is even")
+else:
+    print(" the number is odd")
+    
+if number>100:
+    print("the number is greater than 100")
+    
+
